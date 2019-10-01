c0b07439eddc3eff435611185e894b926dd2e0fd
Stephen Adams <tsadams@gmail.com>
2019-10-01 10:10:42 -0400

    Fix consumer

--- app.py
+++ app.py
@@ -20,23 +20,19 @@ def main():
 
     config.log_config()
 
-    validation_consumer = consume.init_validation_consumer()
-    egress_consumer = consume.init_egress_consumer()
+    consumer = consume.init_consumer()
     global producer
     producer = produce.init_producer()
 
     while True:
-        for data in validation_consumer:
+        for data in consumer:
             try:
-                check_validation(data.value)
+                if data.topic == config.CONSUME_TOPIC:
+                    check_validation(data.value)
+                else:
+                    produce_available(data.value)
             except Exception:
                 logger.exception("An error occurred during message processing")
-
-        for data in egress_consumer:
-            try:
-                produce_available(data.value)
-            except Exception:
-                logger.exception("An error occurred during egress message processing")
 
         producer.flush()
 
@@ -47,11 +43,11 @@ def produce_available(msg):
     logger.debug("Incoming Egress Message Content: %s", msg)
     tracker_msg = msgs.create_msg(msg, "received", "received egress message")
     send_message(config.TRACKER_TOPIC, tracker_msg)
-    platform_metadata = msg.get("platform_metadata")
+    platform_metadata = msg.pop("platform_metadata")
     available_message = {**msg, **platform_metadata}
     logger.debug("Outgoing Egress Message Contents: %s", available_message)
     send_message(config.ANNOUNCER_TOPIC, available_message)
-    tracker_msg = msgs.create_msg(msg, "success", "sent message to %s", config.ANNOUNCER_TOPIC)
+    tracker_msg = msgs.create_msg(msg, "success", "sent message to platform.upload.available")
     send_message(config.TRACKER_TOPIC, tracker_msg)
     logger.info("Sent success message to %s for request %s", config.ANNOUNCER_TOPIC, available_message.get("request_id"))
 
@@ -64,7 +60,7 @@ def check_validation(msg):
     elif msg.get("validation") == "failure":
         try:
             aws.copy(msg.get("request_id"))
-            tracker_msg = msgs.create_msg(msg, "success", "copied failed payload to %s bucket", config.REJECT_BUCKET)
+            tracker_msg = msgs.create_msg(msg, "success", "copied failed payload to reject bucket")
             send_message(config.TRACKER_TOPIC, tracker_msg)
         except ClientError:
             logger.exception("Unable to move %s to %s bucket", config.REJECT_BUCKET, msg.get("request_id"))
