90ff495c7e1a4c34c64ff054dd8d3e662eb843ab
Stephen <tsadams@gmail.com>
2021-02-24 12:37:43 -0500

    Switch to haberdasher for logs (#37)
    * feat: add haberdasher to image
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * feat: add haberdasher env vars to deploy file
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * feat: remove unneeded modules
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * feat: remove cloudwatch from logging
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * format: new line at end of file
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * fix: remove log group
    
    Not needed with haberdasher
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,12 @@ COPY default_map.yaml /opt/app-root/src/default_map.yaml
 
 RUN pip3 install --upgrade pip && pip3 install .
 
+RUN curl -L -o /usr/bin/haberdasher \
+https://github.com/RedHatInsights/haberdasher/releases/latest/download/haberdasher_linux_amd64 && \
+chmod 755 /usr/bin/haberdasher
+
 USER 1001
 
-ENTRYPOINT ["storage_broker"]
+ENTRYPOINT ["/usr/bin/haberdasher"]
+
+CMD ["storage_broker"]
--- src/storage_broker/utils/broker_logging.py
+++ src/storage_broker/utils/broker_logging.py
@@ -2,36 +2,15 @@ import os
 import sys
 import logging
 
-import watchtower
-
-from logstash_formatter import LogstashFormatterV1
-from boto3.session import Session
 
 from src.storage_broker.utils import config
-
-
-def config_cloudwatch(logger):
-    CW_SESSION = Session(
-        aws_access_key_id=config.CW_AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=config.CW_AWS_SECRET_ACCESS_KEY,
-        region_name=config.AWS_REGION,
-    )
-    cw_handler = watchtower.CloudWatchLogHandler(
-        boto3_session=CW_SESSION,
-        log_group=config.LOG_GROUP,
-        stream_name=config.HOSTNAME,
-        create_log_group=False,
-    )
-    cw_handler.setFormatter(LogstashFormatterV1())
-    logger.addHandler(cw_handler)
 
 
 def initialize_logging():
     kafkalogger = logging.getLogger("kafka")
     kafkalogger.setLevel("ERROR")
     if any("OPENSHIFT" in k for k in os.environ):
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(LogstashFormatterV1())
+        handler = logging.StreamHandler(sys.stderr)
         logging.root.setLevel(os.getenv("LOG_LEVEL", "INFO"))
         logging.root.addHandler(handler)
     else:
@@ -41,11 +20,5 @@ def initialize_logging():
         )
 
     logger = logging.getLogger(config.APP_NAME)
-
-    if config.CW_AWS_ACCESS_KEY_ID and config.CW_AWS_SECRET_ACCESS_KEY:
-        config_cloudwatch(logger)
-        logger.info("Cloudwatch Logging Enabled")
-    else:
-        logger.info("Cloudwatch Logging Disabled")
 
     return logger
